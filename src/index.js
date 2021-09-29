75afcd7e2539a8c7653192e9b81c67511ee0c493
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2021-09-29 15:46:47 +0300

    2 commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+
+import API from './js/fetchCountries'
+import countryItemsTemplate from './templates/countryItem.hbs';
+import countriesListTemplate from './templates/countriesList.hbs';
+import refs from './js/refs'
+
+const DEBOUNCE_DELAY = 300;
+const debounce = require('lodash.debounce');
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+    return {
+        searchForm: document.querySelector('#search-form'),
+        countryList: document.querySelector('#country-list'),
+        searchInput: document.querySelector('.search__input'),
+    };
+}
